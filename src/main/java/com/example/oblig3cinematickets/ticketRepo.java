0cd3827eaafb878d25803dbc4060ab3d7be8e483
nataliaberaki <naber1286@oslomet.no>
2024-04-25 14:30:01 +0200

    changes to ticket repo and ticket controller

--- src/main/java/com/example/oblig3cinematickets/ticketRepo.java
+++ src/main/java/com/example/oblig3cinematickets/ticketRepo.java
@@ -1,6 +1,7 @@
 package com.example.oblig3cinematickets;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/oblig3cinematickets/ticketsController.java
+++ src/main/java/com/example/oblig3cinematickets/ticketsController.java
@@ -1,11 +1,29 @@
 package com.example.oblig3cinematickets;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ticketsController {
 
+    @Autowired
+    private ticketRepo rep;
 
+    @PostMapping("/save")
+    public void saveTicket(Ticket ticket){
+        rep.saveTickets(ticket);
+    }
+
+    @GetMapping("/showAllTickets")
+    public List<Ticket> showTicket(){
+        return rep.getAllTickets();
+    }
+
+    @GetMapping("/deleteAllTickets")
+    public void deleteAllTickets(){
+        rep.deleteAllTickets();
+    }
 
 }
